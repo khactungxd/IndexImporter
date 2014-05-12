9801067a9b7c27e3646e23c434cb0844bbea330b
Tunglk <luongkhactung@gmail.com>
2014-05-12 10:20:09 +0700

    Signed-off-by: Tunglk <luongkhactung@gmail.com>

--- config/index.js
+++ config/index.js
@@ -2,5 +2,6 @@ module.exports = {
   PUBLIC_DIR : "./public/xml/",
   HTML_INDEX_DIR : "/mnt/data/Index/",
 //  HTML_INDEX_DIR : "./HTML/",
-  WEBSERVICE_URL : "https://wackler-int.oxseed.com/oxseed/orderbase/wackler/order"
+  WEBSERVICE_URL : "https://wackler-int.oxseed.com/oxseed/orderbase/wackler/order",
+  FILES_PER_A_THREAD : 100
 }
